1fd0a8f8c7a578f8ef055b19ba8fe3203332a43d
Mario Ochoa <mario8ato@hotmail.com>
2018-12-29 23:21:41 -0600

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,53 @@
+require('./config/config');
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+//los app.use se llaman middlewares
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.get('/usuario', function(req, res) {
+    res.json('get usuario');
+});
+
+//POST ES PARA CREAR NUEVOS REGISTROS
+app.post('/usuario', function(req, res) {
+
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+
+    } else {
+        res.json({
+            persona: body
+        });
+    }
+});
+
+//PUT ES PARA ACTUALZAR REGISTROS
+app.put('/usuario/:id', function(req, res) {
+    //ese id despues del params se refiera al id de arriba (/:id)
+    let id = req.params.id;
+    res.json({
+        id
+    });
+});
+
+app.delete('/usuario', function(req, res) {
+    res.json('delete usuario');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto: ', 3000);
+});
