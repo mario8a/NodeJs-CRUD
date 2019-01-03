747f3f97b30a89bcd2f6ec10e22cf1f6e393ff9a
Mario Ochoa <mario8ato@hotmail.com>
2019-01-02 20:11:57 -0600

    variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:mario23101996@ds129831.mlab.com:29831/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 //esto es para cambiar la cadena de conexion de localhost
